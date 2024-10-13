3c14c9c10b4386ff3e89486b88f8e577332823fb
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2024-10-13 12:45:45 +0530

    Lectures

---
+++ Lecture 52/LCA_In_BST.cpp
@@ -0,0 +1,59 @@
+
+#include<iostream>
+using  namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        if (root == NULL) {
+            return NULL;
+        }
+
+
+        if ((root->val > p->val) and (root->val > q->val)) {
+            //LCA is in the left half.
+            return lowestCommonAncestor(root->left, p, q);
+        }
+
+        if ((root->val < p->val) and (root->val < q->val)) {
+            //LCA is in the right half.
+            return lowestCommonAncestor(root->right, p, q);
+        }
+
+
+        //Any other Scenario:
+        return root;
+
+    }
+};
+
+
+int main() {
+
+}
---
+++ Lecture 52/Test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using  namespace std;
+
+int main() {
+	int x = 10;
+
+	int y{10};//Uniform
+	int z = {12};
+
+	int p(10);
+
+}
